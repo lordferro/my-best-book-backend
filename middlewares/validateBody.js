5bd16690db4ba463c8b2557db186e322cd614118
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-09-05 15:07:45 +0200

    add normalizer first Letter

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -8,7 +8,14 @@ const validateBody = (schema) => {
     }
     if (req.body.name.toLowerCase().includes(" and "))
       next(HttpError(404, "Sorry, but you cant use stand alone word 'and'"));
+    const handlerArray = req.body.name.split(" ");
 
+    const resultArray = [];
+
+    for (const item of handlerArray) {
+      if (item) resultArray.push(item.charAt(0).toUpperCase() + item.slice(1));
+    }
+    req.body.name = resultArray.join(" ");
     next();
   };
   return func;
